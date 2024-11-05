a4d546a453a5ad7af40799f2036976eeb25fa32b
Xiaoxiang(Anna) Ma <xim006@ucsd.edu>
2024-11-04 19:52:08 -0800

    Update Remaining.tsx

--- client/src/components/Remaining.tsx
+++ client/src/components/Remaining.tsx
@@ -4,17 +4,22 @@ import { AppContext } from "../context/AppContext";
 const Remaining = () => {
   const { expenses, budget } = useContext(AppContext);
 
-  const totalExpenses = expenses.reduce((total, item) => {
-    return (total = total + item.cost);
-  }, 0);
+  const totalExpenses = expenses.reduce((total, item) => total + item.cost, 0);
+  const remaining = budget - totalExpenses;
+  const alertType = remaining < 0 ? "alert-danger" : "alert-success";
 
-  const alertType = totalExpenses > budget ? "alert-danger" : "alert-success"
+  useEffect(() => {
+    if (remaining < 0) {
+      alert("You have exceeded your budget!");
+    }
+  }, [remaining]);
 
   return (
     <div className={`alert ${alertType}`}>
-      <span>Remaining: ${budget - totalExpenses}</span>
+      <span>Remaining: ${remaining}</span>
     </div>
   );
 };
 
 export default Remaining;
+

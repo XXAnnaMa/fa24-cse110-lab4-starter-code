e74f31d23bda62016c07a25948805338ba44790a
Xiaoxiang(Anna) Ma <xim006@ucsd.edu>
2024-11-04 19:47:15 -0800

    Update ExpenseItem.tsx

--- client/src/components/Expense/ExpenseItem.tsx
+++ client/src/components/Expense/ExpenseItem.tsx
@@ -3,29 +3,22 @@ import { Expense } from "../../types/types";
 import { AppContext } from "../../context/AppContext";
 import { deleteExpense } from "../../utils/expense-utils";
 
-const ExpenseItem = (currentExpense: Expense) => {
-  // Exercise: Consume the AppContext here
-  const {expenses, setExpenses} = useContext(AppContext);
+const ExpenseItem = ({ id, description, cost }: Expense) => {
+  const { expenses, setExpenses } = useContext(AppContext);
 
-  const handleDeleteExpense = (currentExpense: Expense) => {
-    // Exercise: Remove expense from expenses context array
-
-    deleteExpense(currentExpense.id);
-    
-    const newExpenses = expenses.filter((expense) => expense.id != currentExpense.id);
-    setExpenses(newExpenses);
-    
+  const handleDeleteExpense = () => {
+    deleteExpense(id);
+    setExpenses(expenses.filter((expense) => expense.id !== id));
   };
 
   return (
     <li className="list-group-item d-flex justify-content-between align-items-center">
-      <div>{currentExpense.description}</div>
-      <div>${currentExpense.cost}</div>
-      <div>
-        <button onClick={() => handleDeleteExpense(currentExpense)}>x</button>
-      </div>
+      <div>{description}</div>
+      <div>${cost}</div>
+      <button onClick={handleDeleteExpense} className="btn btn-danger btn-sm">
+        x
+      </button>
     </li>
   );
 };
 
-export default ExpenseItem;
